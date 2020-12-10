e2f1cdd8af8dcd773b28733a2bdb2dc1ea36959b
bx <xylagbx@icloud.com>
2020-12-10 22:57:48 +0800

    AdBlock

--- 提取特定行.py
+++ 提取特定行.py
@@ -1,5 +1,6 @@
 import re
 
+
 def Extract_Line(file1, file2, words):
     """将 file1 中包含 words 的行提取到 file2 中"""
     try:
@@ -11,8 +12,11 @@ def Extract_Line(file1, file2, words):
         file1.close()
         file2.close()
 
-org = open(r'原始文件/org.txt', 'r')
-direct = open(r'原始文件/Direct.txt', 'w')
-proxy = open(r'原始文件/Proxy.txt', 'w')
 
-Extract_Line(org, direct, '')
+org = open(r'PROXY/IP-CIDR.txt', 'r')
+ipv4 = open(r'PROXY/ipv4.txt', 'w')
+ipv6 = open(r'PROXY/ipv6.txt', 'w')
+
+Extract_Line(org, ipv4, 'IP-CIDR,')
+org = open(r'PROXY/IP-CIDR.txt', 'r')
+Extract_Line(org, ipv6, 'IP-CIDR6,')
