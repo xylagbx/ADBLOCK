f44d240d2e556b8580a0a91a67e195330d5e2a2b
bx <xylagbx@icloud.com>
2021-01-05 03:20:59 +0800

    AdBlock

--- 排除.py
+++ 排除.py
@@ -1,4 +1,6 @@
 import re
+
+
 def del_line(file, words):
     """删除具有特定内容的行"""
     with file as f:
@@ -17,20 +19,9 @@ def del_line(file, words):
 #     del_line(open(r'PRuleSet.list', 'r+'), eachline)
 
 """从direct中排除reject"""
-RuleSet = open(r'RuleSet.list', 'r')
+RuleSet = open(
+    r'原始文件/Rule_set/anti-ad.txt', 'r')
 alllines = RuleSet.readlines()
 RuleSet.close()
 for eachline in alllines:
-    del_line(open(r'DRuleSet.list', 'r+'), eachline)
-
-"""从proxy中排除reject，direct"""
-# RuleSet = open(r'RuleSet.list', 'r')
-# alllines = RuleSet.readlines()
-# RuleSet.close()
-# for eachline in alllines:
-#     del_line(open(r'PRuleSet.list', 'r+'), eachline)
-# DRuleSet = open(r'DRuleSet.list', 'r')
-# alllines = DRuleSet.readlines()
-# DRuleSet.close()
-# for eachline in alllines:
-#     del_line(open(r'PRuleSet.list', 'r+'), eachline)
+    del_line(open(r'原始文件/myblack的副本.txt', 'r+'), eachline)
